34ec55c6b738a4be8fc13c11e24b6c7d43621b4a
Andrii <andriisiuta@gmail.com>
2023-02-15 17:18:12 +0200

    remove unused modules

--- tech-test/src/app/app.module.ts
+++ tech-test/src/app/app.module.ts
@@ -4,10 +4,7 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
-import {UiKitModule} from './ui-kit/ui-kit.module';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {TuiActionModule, TuiCheckboxModule, TuiInputModule, TuiTextAreaModule} from '@taiga-ui/kit';
-import {TuiButtonModule} from '@taiga-ui/core';
 
 @NgModule({
   declarations: [
@@ -19,12 +16,6 @@ import {TuiButtonModule} from '@taiga-ui/core';
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    TuiActionModule,
-    UiKitModule,
-    TuiInputModule,
-    TuiTextAreaModule,
-    TuiCheckboxModule,
-    TuiButtonModule,
   ],
   providers: [HttpClient],
   bootstrap: [AppComponent]
